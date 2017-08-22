158ab97217c32fe84fc885b03bbfdd9381b8b143
R.Sai Sravya <saisravyaravi@gmail.com>
2017-08-22 13:13:14 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/articleone', function (req, res) {
+app.get('/article_one', function (req, res) {
   res.sendFile('article1 requested');
 });
 app.get('/arttwo', function (req, res) {
